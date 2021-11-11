f66019a3a51e677d984a3f02051d29a488ebd6a7
eddyone7 <eddy.giron@unah.hn>
2021-11-10 23:59:32 -0600

    Estructura

--- frontend/EventServe/src/app/app.module.ts
+++ frontend/EventServe/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './componentes/users/users.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { DashboardComponent } from './componentes/users/dashboard/dashboard.component';
+import { EventComponent } from './componentes/users/event/event.component';
+import { MyeventsComponent } from './componentes/users/myevents/myevents.component';
+import { ProfileComponent } from './componentes/users/profile/profile.component';
+import { StatisticsComponent } from './componentes/users/statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    LoginComponent,
+    DashboardComponent,
+    EventComponent,
+    MyeventsComponent,
+    ProfileComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
